2f656fde84d6e46807425fc48c6654eeef5071a4
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-02 13:29:52 +0100

    Initial Commit

---
+++ Minuteur/MinuteurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinuteurApp.swift
+//  Minuteur
+//
+//  Created by  Ixart on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MinuteurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
