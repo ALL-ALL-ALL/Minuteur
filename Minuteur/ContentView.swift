d8dbce45244a02ed6fe1a4ff89b268d8209ac5a4
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-02 15:34:35 +0100

    1er changemet du fichier

--- Minuteur/ContentView.swift
+++ Minuteur/ContentView.swift
@@ -10,14 +10,124 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            VStack{
+                Text("minuteur")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Image(systemName: "timer")
+                    .font(.largeTitle)
+                    .bold()
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                    .padding(.bottom, 100) // Ajoutez un espace en haut du texte
+
+                
+            }
+                
+                
+                
+                
+                
+                
+                
+
+            
+            
+            
+            
+            
+            VStack{
+                
+                
+                
+
+                
+                Circle()
+                    .stroke(lineWidth: 20)
+                    .foregroundColor(.pink)
+                
+                
+                Text("12.9")
+                    .padding(.top, -190) // Ajoutez un espace en haut du texte
+                    .font(.largeTitle)
+                    .bold()
+                
+
+                
+                
+                
+                    
+                
+                
+                HStack{
+
+
+
+                    Button("play") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(
+                        RoundedRectangle(
+                            cornerRadius: 20,
+                            style: .continuous
+                        )
+                        .fill(.pink)
+                    )
+                    
+                    
+                  
+                    
+                    
+
+                    
+                    Button("replay") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(
+                        RoundedRectangle(
+                            cornerRadius: 20,
+                            style: .continuous
+                        )
+                        .fill(.pink)
+                    )
+
+                 
+         
+                } // FIN HSTACK
+                .padding(.bottom, 100) // Ajoutez un espace en haut du texte
+
+                
+                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+                
+                
+                
+ 
+            } // FIN ZSTACK
+
+        } // FIN VSTACK
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         .padding()
-    }
-}
+    } // FIN BODY
+} // FIN STRUCTURE
 
 #Preview {
     ContentView()
