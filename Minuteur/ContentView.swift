855a9a2963b8f89b90cc6a7ea0d3cc3e163aa6b2
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-12 19:29:42 +0100

    j'ai tout refais en y allant plus lentement il manque plus qu le slider ne suivent plus le compte a rebour

--- Minuteur/ContentView.swift
+++ Minuteur/ContentView.swift
@@ -8,47 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var Pause = true
-    @State private var tempsecoule = 0.0
-    @State private var comptearebours = 20.0 // Temps initial du compte à rebours
-    @State private var encour = false
-    
+    @State var slider = 10.0
+
+    @State private var enLecture = false // pour que l'image change de lecture à pause
+    @State var progress: Double = 0
+    @State private var remainingTime = 10.0 // Temps initial du compte à rebours
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
-    func startCountdown() {
-        tempsecoule = 0 // Réinitialiser le temps écoulé
-          // Vous pouvez également ajouter d'autres logiques ici si nécessaire
-      }
-      
-      func stopCountdown() {
-          // Arrêtez le compte à rebours
-      }
-      
-      func resetCountdown() {
-          stopCountdown() // Arrêtez d'abord le compte à rebours
-          tempsecoule = 0 // Réinitialiser le temps écoulé
-      }
-  
-
-
     
     
-    func timeString(time: Double) -> String {
-            let minutes = Int(time) / 60
-            let seconds = Int(time) % 60
-            return String(format: "%02d:%02d", minutes, seconds)
-        }
+    
     
     
     
     var body: some View {
+        
         VStack {
             VStack{
                 Text("Minuteur")
                     .font(.largeTitle)
                     .bold()
                     .padding(.bottom, 5) // Ajoute une marge inférieure pour déplacer les boutons plus bas
-
+                
                 
                 Image(systemName: "timer")
                     .font(.largeTitle)
@@ -56,63 +37,79 @@ struct ContentView: View {
                     .imageScale(.large)
                     .foregroundStyle(.red)
                     .padding(.bottom, 100) // Ajoutez un espace en haut du texte
-
+                
                 
             }// FIN VSTACK
             ZStack{
-               
+                
                 
                 Circle()
-                        .trim(from: 0, to: CGFloat(tempsecoule / comptearebours)) // Progression du cercle en fonction du temps écoulé
-                        .foregroundColor(.pink)
+                    .stroke(style: StrokeStyle(lineWidth: 20, lineJoin: .round))
+                    .padding(50)
                 
-                        .padding(-60)
-         
+                
+                Circle()
+                    .trim(from: 0, to: CGFloat(progress / slider))
+                    .stroke(.pink, style: StrokeStyle(lineWidth: 20,lineCap: .round, lineJoin: .round))
+                    .padding(50)
+                
+                Text("\(Int(remainingTime))")
 
+                
+                
+                
+                
+                
+                
+                
+                
             }// FIN ZSTACK
             
-
-                      
-            Text(timeString(time: comptearebours - tempsecoule))
-
-                .padding(.top, -130)
-                .font(.title)
-                .bold()
-
+            
+            .padding(.top, -130)
+            .font(.title)
+            .bold()
+            
             Spacer().frame(height: 100) // Réduit la largeur de l'espace entre les boutons
-
+            
             HStack{
-              
+                
                 Button(action: {
-                    Pause.toggle()
-
+                    enLecture.toggle()
+                    
+                    
                 }) {
+                    
+                    
+                    
                     HStack {
-                        Image(systemName: Pause ? "play" : "pause") // Changer l'image en fonction de Pause
-                            .font(.title) // taille du symbole
+                        Image(systemName: enLecture ? "pause" : "play")
+                            .font(.title) //
                         
-                        Text(Pause ? "Play" : "Pause") // Changer le texte en fonction de Pause
+                        Text(enLecture ? "Pause" : "Play")
                             .bold()
                     }// FIN HSTACK
                     
                     .padding() // Ajoutez un padding autour du contenu du bouton
                     .frame(maxWidth: 150) // Étire le bouton de droite à gauche
-
+                    
                     .background(Color.black) // Couleur de fond du bouton
                     .foregroundColor(.white) // Couleur du texte
                     .cornerRadius(50) // Coins arrondis du bouton
                 } // FIN BUTTON
                 
                 Spacer().frame(width: 50) // Réduit  l'espace entre les boutons
-
+                
+                
+                
+                
                 Button(action: {
-                                   encour.toggle()
-                                   if encour {
-                                       startCountdown() // Commencer le compte à rebours lorsque le bouton "Play" est pressé
-                                   } else {
-                                       stopCountdown() // Arrêter le compte à rebours lorsque le bouton "Pause" est pressé
-                                   }
-                               }) {
+                    remainingTime = slider
+
+                    
+                }) {
+                    
+                    
                     HStack {
                         Image(systemName: "arrow.counterclockwise.circle")
                             .font(.title) // taille du symbole
@@ -123,20 +120,27 @@ struct ContentView: View {
                     
                     .padding() // Ajoutez un padding autour du contenu du bouton
                     .frame(maxWidth: 150) // Étire le bouton de droite à gauche
-
+                    
                     .background(Color.black) // Couleur de fond - bouton-
                     .foregroundColor(.white) // Couleur du texte
                     .cornerRadius(50) // Coins arrondis du bouton
                 } // FIN BUTTON
+                
+                
+                
             } // FIN HASTACK
+            Slider(value: $remainingTime, in: 0...50 )
+            
         } // FIN VSTACK
-                .padding(.bottom, 100) // Ajoutez un espace en haut du texte
-
-        Slider(value:$tempsecoule, in : 0...200)
-            .padding()
-
-            } // FIN  BODY
-        } // FIN STRUCT
+        .onReceive(timer) { _ in
+                    if enLecture && remainingTime > 0 {
+                        remainingTime -= 1
+                        progress = remainingTime
+                    }
+                }
+            
+        }// FIN body
+    } // FIN STRUCT
 
 #Preview {
     ContentView()
